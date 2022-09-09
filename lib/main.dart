b0da8608bb9a8f9f83d2a13123781f9a509049d3
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2022-09-09 15:47:18 +0600

    delet and create statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -12,32 +11,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int x = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Lottery'),
-        ),
-        body: SafeArea(
-          child: Center(
-            child: Text(
-              x.toString(),
-              style: TextStyle(fontSize: 50),
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(onPressed: () {
-          x++;
-          setState(() {
 
-          });
-          print(x.toString());
-          print("I love you.");
-        }),
-      ),
     );
   }
 }
+
+
