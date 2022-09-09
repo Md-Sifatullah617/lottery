fe85c2ae65b8c2bd37879d0d2e9064cd13c6097c
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2022-09-09 16:14:14 +0600

    tjkg

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,39 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int x = 4;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lottery App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(child: Text('Your lottery winning number is $x')),
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(.3)
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+               children: [
+                 Icon(Icons.error, color: Colors.red, size: 35,),
+                 Text('Better luck next time, yor number is $x try again')
+               ], 
+              ),
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: (){
+          
+        },
+        child: Icon(Icons.refresh),
+        ),
+      ),
     );
   }
 }
